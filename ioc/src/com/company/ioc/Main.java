77f2963223dd4f24bb141b4b43047ea044d97a81
Heeju Sim <heeju01226@gmail.com>
2021-09-23 13:59:25 +0900

    DI 주입

--- ioc/src/com/company/ioc/Main.java
+++ ioc/src/com/company/ioc/Main.java
@@ -4,7 +4,10 @@ public class Main {
     public static void main(String[] args) {
         String url = "www.naver.com/books/it?page=10&size=20?name=spring-boot";
         //base 64 encoding
-        IEncoder encoder = new Base64Encoder();
+        //IEncoder encoder = new Base64Encoder();
+        //Encoder encoder = new Encoder();
+        //DI : 외부에서 사용할 객체를 주입받는 것. Encoder라는 본질객체는 건드리지 않고 주입 객체만 바꿔서 사용할 수 있다.
+        Encoder encoder = new Encoder(new Base64Encoder());
         String result = encoder.encode(url);
         //url encoding
         IEncoder urlEncoder = new UrlEncoder();
