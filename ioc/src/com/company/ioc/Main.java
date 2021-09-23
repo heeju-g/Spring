cf5ad41be185a56dac65de0b7182f16b3a45d726
Heeju Sim <heeju01226@gmail.com>
2021-09-23 13:51:57 +0900

    interface만 추가

--- ioc/src/com/company/ioc/Main.java
+++ ioc/src/com/company/ioc/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         String url = "www.naver.com/books/it?page=10&size=20?name=spring-boot";
         //base 64 encoding
-        Encoder encoder = new Encoder();
+        IEncoder encoder = new Base64Encoder();
         String result = encoder.encode(url);
         //url encoding
-        UrlEncoder urlEncoder = new UrlEncoder();
+        IEncoder urlEncoder = new UrlEncoder();
         String urlResult = urlEncoder.encode(url);
         System.out.println(urlResult);
     }
