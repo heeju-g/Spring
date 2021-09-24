ef5c68122758e9111b2f085da631511e8dfe08ed
Heeju Sim <heeju01226@gmail.com>
2021-09-24 14:15:53 +0900

    aop(parameter)

---
+++ aop/src/main/java/com/example/aop/aop/ParameterAop.java
@@ -0,0 +1,33 @@
+package com.example.aop.aop;
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class ParameterAop {
+    @Pointcut("execution(* com.example.aop.controller..*.*(..))")
+    private void cut(){
+
+    }
+    //cut메소드가 실행되기 전에
+    @Before("cut()")
+    public void before(JoinPoint joinPoint){
+        //메소드에 들어가는 매개변수들의 배열
+        Object[] args = joinPoint.getArgs();
+
+        for(Object obj : args){
+            System.out.println("type: "+obj.getClass().getSimpleName());
+            System.out.println("value: "+obj);
+        }
+    }
+    @AfterReturning(value = "cut()",returning = "returnObj")
+    public void afterReturn(JoinPoint joinPoint, Object returnObj){
+        System.out.println("returnObj");
+        System.out.println(returnObj);
+    }
+}
