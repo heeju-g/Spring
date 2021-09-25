9d7f2a83f222447cd8990c3ac2a86660edd9a91c
Heeju Sim <heeju01226@gmail.com>
2021-09-25 15:28:49 +0900

    create object mapper

--- object-mapper/src/main/java/Main.java
+++ object-mapper/src/main/java/Main.java
@@ -1,5 +1,31 @@
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dto.Car;
+import dto.User;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
     public static void main(String args[]){
         System.out.println("main");
+        ObjectMapper objectMapper = new ObjectMapper();
+        User user = new User();
+        user.setName("홍길동");
+        user.setAge(10);
+
+        Car car1 = new Car();
+        car1.setName("k5");
+        car1.setCarNumber("11가1111");
+        car1.setType("sedan");
+
+        Car car2 = new Car();
+        car2.setName("q5");
+        car2.setCarNumber("22가1111");
+        car2.setType("suv");
+
+        List<Car> carList = Arrays.asList(car1,car2);
+        user.setCars(carList);
+
+        System.out.println(user);
     }
 }
