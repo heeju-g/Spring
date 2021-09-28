5626c42142d01de6df1242b21e030cb0df96b7a3
Heeju Sim <heeju01226@gmail.com>
2021-09-28 14:20:01 +0900

    bindingResult

--- validation/src/main/java/com/example/validation/dto/User.java
+++ validation/src/main/java/com/example/validation/dto/User.java
@@ -9,7 +9,7 @@ public class User {
 
     @Email
     private String email;
-    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$")
+    @Pattern(regexp = "^\\d{2,3}-\\d{3,4}-\\d{4}$", message = "핸드폰 번호 양식과 맞지 않습니다. 01x-xxx(x)-xxxx")
     private String phoneNumber;
 
     public String getName() {
