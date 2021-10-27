987122a956921ce47f0f15a82dfcfcb9e8a521e8
Heeju Sim <heeju01226@gmail.com>
2021-10-27 19:16:05 +0900

    create Classes (Calculator/ICalculator/KrwCalculator)

--- calculator/src/main/java/Main.java
+++ calculator/src/main/java/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String args[]){
         System.out.println("JUnit");
+
+
+        Calculator calculator = new Calculator(new KrwCalculator());
+
     }
 }
