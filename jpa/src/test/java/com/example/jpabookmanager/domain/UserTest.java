06fdbb5acdd4c35ae9cb6047b540f15989b61b52
Heeju Sim <heeju01226@gmail.com>
2021-11-05 17:39:44 +0900

    UserRepository 테스트(crud)

--- jpa/src/test/java/com/example/jpabookmanager/domain/UserTest.java
+++ jpa/src/test/java/com/example/jpabookmanager/domain/UserTest.java
@@ -2,6 +2,8 @@ package com.example.jpabookmanager.domain;
 
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserTest {
@@ -11,6 +13,16 @@ class UserTest {
         User user = new User();
         user.setEmail("heeju@gmail.com");
         user.setName("heeju");
+        user.setCreatedAt(LocalDateTime.now());
+        user.setUpdatedAt(LocalDateTime.now());
+
+        User user1 = new User(null, "heeju","heeju@gmail.com", LocalDateTime.now(), LocalDateTime.now());
+        User user2 = new User("heeju","heeju@gmail.com");
+
+        User user3 = User.builder()
+                        .name("heeju")
+                                .email("heeju@gmail.com")
+                                        . build();
         System.out.println(">>>"+user);
     }
 }
---
+++ jpa/src/test/java/com/example/jpabookmanager/repository/UserRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.example.jpabookmanager.repository;
+
+import com.example.jpabookmanager.domain.User;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class UserRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+    
+    @Test
+    void crud(){
+        userRepository.save(new User());
+        //유저 테이블 모든 데이터를 리스트 형식으로 가져오기
+        System.out.println(">>>"+userRepository.findAll());
+    }
+}
