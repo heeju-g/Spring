b58ab42401c8ed0587a725defb89d79875225c9f
Heeju Sim <heeju01226@gmail.com>
2021-11-08 12:44:51 +0900

    repository interface 메소드들 사용

--- jpa/src/test/java/com/example/jpabookmanager/repository/UserRepositoryTest.java
+++ jpa/src/test/java/com/example/jpabookmanager/repository/UserRepositoryTest.java
@@ -1,11 +1,15 @@
 package com.example.jpabookmanager.repository;
 
 import com.example.jpabookmanager.domain.User;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Sort;
 
-import static org.junit.jupiter.api.Assertions.*;
+import javax.transaction.Transactional;
+import java.util.List;
+
 
 @SpringBootTest
 class UserRepositoryTest {
@@ -13,9 +17,29 @@ class UserRepositoryTest {
     private UserRepository userRepository;
     
     @Test
+    @Transactional
     void crud(){
+        /*
         userRepository.save(new User());
-        //유저 테이블 모든 데이터를 리스트 형식으로 가져오기
+        유저 테이블 모든 데이터를 리스트 형식으로 가져오기
         userRepository.findAll().forEach(System.out::println);
+         */
+        /*name 기준으로 역순 정렬.
+        List<User> users = userRepository.findAll(Sort.by(Sort.Direction.DESC,"name"));
+         */
+        /* Lists는 테스트용 라이브러리에 존재.
+        List<User> users = userRepository.findAllById(Lists.newArrayList(1L,3L,5L));
+         */
+        /*
+        User user1 = new User("jack","jack@gmail.com");
+        User user2 = new User("jacky","jacky@gmail.com");
+        userRepository.saveAll(Lists.newArrayList(user1,user2));
+        List<User> users = userRepository.findAll();
+        users.forEach(System.out::println);
+         */
+        //세션 유지하려고 @Transactional사용
+        User user = userRepository.getById(1L);
+        System.out.println(user);
+
     }
 }
--- jpa/src/test/resources/data.sql
+++ jpa/src/test/resources/data.sql
@@ -10,5 +10,5 @@ insert into user('id','name','email','created_at','updated_at') values (3,'jungm
 call next value for hibernate_sequence;
 insert into user('id','name','email','created_at','updated_at') values (4,'jimin','jimin@gmail.com',now(),now());
 
-call next value for hibernate_sequence;
+--call next value for hibernate_sequence;
 insert into user('id','name','email','created_at','updated_at') values (5,'heeju','heeju02@gmail.com',now(),now());
